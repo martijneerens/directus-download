20e3e8e9d9eb5061640e28fa5f8e00258482aafb
Hay Kranen <huskyr@gmail.com>
2016-03-22 18:11:55 +0100

    adding option to skip existing files

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ fbdownload({
     bookId : 'your-book-id-here',
     dataPath : 'data.json',
     mediaPath : './media/',
+    skipExistingFiles : true,
     callback : function() {
         console.log('ready');
     }
--- fieldbook-download.js
+++ fieldbook-download.js
@@ -5,6 +5,11 @@ var Download = require('download');
 var async = require('async');
 
 var attachmentRegex = /https?:\/\/fieldbook.com\/attachments/g;
+var defaultOpts = {
+    dataPath : 'data.json',
+    mediaPath : './media/',
+    skipExistingFiles : true
+};
 var opts = {};
 
 function getBook(bookId, cb) {
@@ -19,6 +24,23 @@ function getBook(bookId, cb) {
     dataplugger.load(cb);
 }
 
+function fileExists(filename, cb) {
+    if (!opts.skipExistingFiles) {
+        cb(false);
+        return;
+    }
+
+    fs.stat(filename, (err, stat) => {
+        if (err === null) {
+            cb(true);
+        } else if (err.code === 'ENOENT') {
+            cb(false);
+        } else {
+            throw err;
+        }
+    });
+}
+
 function downloadMediaFromRecords(records, finalCallback) {
     var downloads = [];
 
@@ -29,11 +51,20 @@ function downloadMediaFromRecords(records, finalCallback) {
             if (attachmentRegex.test(val)) {
                 downloads.push(function(callback) {
                     var filename = val.split('/').slice(-2).join('-');
-                    new Download().get(val).dest(opts.mediaPath).rename(filename).run(() => {
-                        console.log("Downloaded " + filename);
-                        callback();
+
+                    fileExists(opts.mediaPath + filename, (exists) => {
+                        if (!exists) {
+                            new Download().get(val).dest(opts.mediaPath).rename(filename).run(() => {
+                                console.log("Downloaded " + filename);
+                                callback();
+                            });
+                        } else {
+                            console.log("Skipping file, exists: " + filename);
+                            callback();
+                        }
+
+                        record[key] = 'media/' + filename;
                     });
-                    record[key] = 'media/' + filename;
                 });
             }
         });
@@ -45,7 +76,7 @@ function downloadMediaFromRecords(records, finalCallback) {
 }
 
 function download(userOpts) {
-    opts = userOpts;
+    opts = _.extend(defaultOpts, userOpts);
 
     console.log("Getting " + opts.bookId);
 
