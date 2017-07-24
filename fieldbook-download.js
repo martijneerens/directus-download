50536e0919af5f35a9fe66966d65f422cb3992c7
Hay Kranen <huskyr@gmail.com>
2017-07-24 15:31:50 +0200

    Seems to work

--- fieldbook-download.js
+++ fieldbook-download.js
@@ -5,6 +5,8 @@ const async = require('async');
 const Papa = require('papaparse');
 
 const attachmentRegex = /https?:\/\/fieldbook.com\/attachments/;
+const MEDIA_URL_KEY = '$mediaUrl$';
+const MEDIA_FILENAME_KEY = '$mediaFilename$';
 
 const defaultOpts = {
     dataPath : 'data.json',
@@ -18,6 +20,8 @@ class FieldbookDownload {
     constructor(bookId, opts) {
         this.bookId = bookId;
         this.opts = Object.assign({}, defaultOpts, opts);
+        this.media = [];
+        this.book = {};
     }
 
     getBook(callback) {
@@ -49,69 +53,67 @@ class FieldbookDownload {
         });
     }
 
-    downloadMediaFromBook(book, callback) {
+    // Check for Fieldbook attachment links, transform them to local urls
+    // and add a downloadable link to the book
+    parseMedia(book) {
+        this.book = book;
+
+        // Can't we decrease these three loops someway?
+        for (let sheetKey in book) {
+            let sheet = book[sheetKey];
+
+            for (let record of sheet) {
+                for (let key in record) {
+                    let val = record[key];
+
+                    if (!attachmentRegex.test(val)) {
+                        continue;
+                    }
+
+                    // We've got a media attachment, transform to something
+                    // new, and add the old url to the book
+                    const externalUrl = val;
+                    const filename = externalUrl.split('/').slice(-2).join('-');
+                    const localPath = `${this.opts.mediaPath}${filename}`;
+
+                    record[MEDIA_URL_KEY + key] = externalUrl;
+                    record[MEDIA_FILENAME_KEY + key] = filename;
+                    record[key] = localPath;
+
+                    this.media.push({ externalUrl, filename, localPath });
+                }
+            }
+        }
+    }
+
+    downloadMedia() {
         let downloads = [];
 
-        for (let key in book) {
-            let sheet = book[key];
-
-            console.log(`Going to download sheet ${key}`);
-
+        for (let media of this.media) {
             downloads.push((downloadCallback) => {
-                this.downloadMediaFromSheet(sheet, downloadCallback);
+                this.fileExists(media.localPath, (pathExists) => {
+                    if (pathExists) {
+                        console.log(`Skipping ${media.filename}, exists`);
+                        downloadCallback();
+                    } else {
+                        console.log(`Going to download ${media.externalUrl}`);
+
+                        new Download().
+                            get(media.externalUrl).
+                            dest(this.opts.mediaPath).
+                            rename(media.filename).
+                            run(() => {
+                                console.log(`Downloaded '${media.externalUrl}'`);
+                                downloadCallback();
+                            });
+                    }
+                });
             });
         }
 
-        async.parallel(downloads, () => {
-            callback(book);
-        });
-    }
-
-    downloadMediaFromSheet(sheet, callback) {
-        let downloads = [];
-
-        for (let record of sheet) {
-            for (let key in record) {
-                let val = record[key];
-
-                if (!attachmentRegex.test(val)) {
-                    continue;
-                }
-
-                const externalUrl = val;
-                const filename = externalUrl.split('/').slice(-2).join('-');
-                const localPath = `${this.opts.mediaPath}${filename}`;
-
-                // Replace the filename in the sheet as well
-                console.log(`New media path for ${key}: '${localPath}'`);
-                record[key] = localPath;
-
-                downloads.push((downloadCallback) => {
-                    this.fileExists(localPath, (pathExists) => {
-                        if (pathExists) {
-                            console.log(`Skipping ${filename}, exists`);
-                            downloadCallback();
-                        } else {
-                            console.log(`Going to download ${externalUrl}`);
-
-                            new Download().
-                                get(externalUrl).
-                                dest(this.opts.mediaPath).
-                                rename(filename).
-                                run(() => {
-                                    console.log(`Downloaded '${externalUrl}'`);
-                                    downloadCallback();
-                                });
-                        }
-                    });
-                });
-            }
-        }
-
-        console.log(`Going to download ${downloads.length} images`);
-
-        async.parallel(downloads, () => {
-            callback(sheet);
+        return new Promise((resolve) => {
+            console.log(`Going to download ${downloads.length} images`);
+            async.parallel(downloads, resolve);
         });
     }
 
@@ -135,12 +137,18 @@ class FieldbookDownload {
         }
     }
 
-    writeBook(book, callback) {
-        let bookJson = this.stringifyJson(book);
+    writeBook() {
+        let bookJson = this.stringifyJson(this.book);
 
-        fs.writeFile(this.opts.dataPath, bookJson, 'utf-8', () => {
-            console.log(`Written JSON file at ${this.opts.dataPath}`);
-            callback();
+        return new Promise((resolve, reject) => {
+            fs.writeFile(this.opts.dataPath, bookJson, 'utf-8', (err, written) => {
+                if (err) {
+                    reject();
+                } else {
+                    console.log(`Written JSON file at ${this.opts.dataPath}`);
+                    resolve();
+                }
+            });
         });
     }
 
@@ -154,9 +162,11 @@ class FieldbookDownload {
                 this.downloadCsv(book);
             }
 
-            this.downloadMediaFromBook(book, (modifiedBook) => {
-                this.writeBook(modifiedBook, this.opts.callback);
-            });
+            this.parseMedia(book);
+
+            this.downloadMedia()
+                .then(this.writeBook.bind(this))
+                .then(this.opts.callback);
         });
     }
 };
