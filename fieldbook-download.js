1abd313cf3aff8a5a149d31d3b193f81dd2d74dd
Hay Kranen <huskyr@gmail.com>
2017-07-24 14:44:14 +0200

    Refactoring

--- fieldbook-download.js
+++ fieldbook-download.js
@@ -1,116 +1,167 @@
-var DataPlugger = require('dataplugger');
-var fs = require('fs');
-var _ = require('underscore');
-var Download = require('download');
-var async = require('async');
-var Papa = require('papaparse');
+const DataPlugger = require('dataplugger');
+const fs = require('fs');
+const Download = require('download');
+const async = require('async');
+const Papa = require('papaparse');
 
-var attachmentRegex = /https?:\/\/fieldbook.com\/attachments/;
-var defaultOpts = {
+const attachmentRegex = /https?:\/\/fieldbook.com\/attachments/;
+
+const defaultOpts = {
     dataPath : 'data.json',
     mediaPath : './media/',
     csvPath : false,
     skipExistingFiles : true,
     prettifyJson : false
 };
-var opts = {};
 
-function getBook(bookId, cb) {
-    var dataplugger = new DataPlugger({
-        'fieldbook' : {
-            book : bookId
-        }
-    });
-
-    dataplugger.setDefaultPlug('fieldbook');
-
-    dataplugger.load(cb);
-}
-
-function fileExists(filename, cb) {
-    if (!opts.skipExistingFiles) {
-        cb(false);
-        return;
+class FieldbookDownload {
+    constructor(bookId, opts) {
+        this.bookId = bookId;
+        this.opts = Object.assign({}, defaultOpts, opts);
     }
 
-    fs.stat(filename, (err, stat) => {
-        if (err === null) {
-            cb(true);
-        } else if (err.code === 'ENOENT') {
-            cb(false);
-        } else {
-            throw err;
+    getBook(callback) {
+        const dataplugger = new DataPlugger({
+            'fieldbook' : {
+                book : this.bookId
+            }
+        });
+
+        dataplugger.setDefaultPlug('fieldbook');
+
+        dataplugger.load(callback);
+    }
+
+    fileExists(filename, callback) {
+        if (!this.opts.skipExistingFiles) {
+            callback(false);
+            return;
         }
-    });
-}
 
-function downloadMediaFromRecords(records, finalCallback) {
-    var downloads = [];
+        fs.stat(filename, (err, stat) => {
+            if (err === null) {
+                callback(true);
+            } else if (err.code === 'ENOENT') {
+                callback(false);
+            } else {
+                throw err;
+            }
+        });
+    }
 
-    console.log("Downloading media records");
+    downloadMediaFromBook(book, callback) {
+        let downloads = [];
 
-    records.forEach((record) => {
-        _.each(record, (val, key) => {
-            if (attachmentRegex.test(val)) {
-                downloads.push(function(callback) {
-                    var filename = val.split('/').slice(-2).join('-');
+        for (let key in book) {
+            let sheet = book[key];
 
-                    fileExists(opts.mediaPath + filename, (exists) => {
-                        if (!exists) {
-                            new Download().get(val).dest(opts.mediaPath).rename(filename).run(() => {
-                                console.log("Downloaded " + filename);
-                                callback();
-                            });
+            console.log(`Going to download sheet ${key}`);
+
+            downloads.push((downloadCallback) => {
+                this.downloadMediaFromSheet(sheet, downloadCallback);
+            });
+        }
+
+        async.parallel(downloads, () => {
+            callback(book);
+        });
+    }
+
+    downloadMediaFromSheet(sheet, callback) {
+        let downloads = [];
+
+        for (let record of sheet) {
+            for (let key in record) {
+                let val = record[key];
+
+                if (!attachmentRegex.test(val)) {
+                    continue;
+                }
+
+                const externalUrl = val;
+                const filename = externalUrl.split('/').slice(-2).join('-');
+                const localPath = `${this.opts.mediaPath}${filename}`;
+
+                // Replace the filename in the sheet as well
+                console.log(`New media path for ${key}: '${localPath}'`);
+                record[key] = localPath;
+
+                downloads.push((downloadCallback) => {
+                    this.fileExists(localPath, (pathExists) => {
+                        if (pathExists) {
+                            console.log(`Skipping ${filename}, exists`);
+                            downloadCallback();
                         } else {
-                            console.log("Skipping file, exists: " + filename);
-                            callback();
-                        }
+                            console.log(`Going to download ${externalUrl}`);
 
-                        record[key] = 'media/' + filename;
+                            new Download().
+                                get(externalUrl).
+                                dest(this.opts.mediaPath).
+                                rename(filename).
+                                run(() => {
+                                    console.log(`Downloaded '${externalUrl}'`);
+                                    downloadCallback();
+                                });
+                        }
                     });
                 });
             }
-        });
-    });
-
-    console.log(downloads.length + " images found");
-
-    async.parallel(downloads, finalCallback);
-}
-
-function downloadCsv(sheet, id) {
-    var csv = Papa.unparse(sheet);
-    var path = opts.csvPath + id + '.csv';
-    fs.writeFile(path, csv, 'utf-8', () => console.log('Downloaded ' + path));
-}
-
-function stringifyJson(data) {
-    if (opts.prettifyJson) {
-        return JSON.stringify(data, null, 4);
-    } else {
-        return JSON.stringify(data);
-    }
-}
-
-function download(userOpts) {
-    opts = _.extend(defaultOpts, userOpts);
-
-    console.log("Getting " + opts.bookId);
-
-    getBook(opts.bookId, (book) => {
-        console.log("Got book");
-
-        if (opts.csvPath) {
-            _.each(book, downloadCsv);
         }
 
-        async.each(_.values(book), downloadMediaFromRecords, () => {
-            fs.writeFile(opts.dataPath, stringifyJson(book), 'utf-8', () => {
-                console.log("Written JSON file at " + opts.dataPath);
-                opts.callback();
+        console.log(`Going to download ${downloads.length} images`);
+
+        async.parallel(downloads, () => {
+            callback(sheet);
+        });
+    }
+
+    downloadCsv(book) {
+        for (let key in book) {
+            let sheet = book[key];
+            const csv = Papa.unparse(sheet);
+            const path = `${this.opts.csvPath}${key}.csv`;
+
+            fs.writeFile(path, csv, 'utf-8', () => {
+                console.log(`CSV downloaded: '${path}'`);
+            });
+        }
+    }
+
+    stringifyJson(data) {
+        if (this.opts.prettifyJson) {
+            return JSON.stringify(data, null, 4);
+        } else {
+            return JSON.stringify(data);
+        }
+    }
+
+    writeBook(book, callback) {
+        let bookJson = this.stringifyJson(book);
+
+        fs.writeFile(this.opts.dataPath, bookJson, 'utf-8', () => {
+            console.log(`Written JSON file at ${this.opts.dataPath}`);
+            callback();
+        });
+    }
+
+    start() {
+        console.log(`Getting ${this.bookId}`);
+
+        this.getBook((book) => {
+            console.log("Got book");
+
+            if (this.opts.csvPath) {
+                this.downloadCsv(book);
+            }
+
+            this.downloadMediaFromBook(book, (modifiedBook) => {
+                this.writeBook(modifiedBook, this.opts.callback);
             });
         });
-    });
-}
+    }
+};
 
-module.exports = download;
+module.exports = function(opts) {
+    const fbdownload = new FieldbookDownload(opts.bookId, opts);
+    fbdownload.start();
+}
