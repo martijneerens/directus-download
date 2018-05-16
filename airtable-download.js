bb525e47ed10ce92af78bee2c614766da9ff9326
Martijn Eerens <martijn@eerens.nl>
2018-05-16 16:05:18 +0200

    load correct script

--- airtable-download.js
+++ airtable-download.js
@@ -16,7 +16,7 @@ const defaultOpts = {
     prettifyJson: false,
 };
 
-class AirtablesDownload {
+class AirtableDownload {
 
     constructor(baseId, apiKey, opts) {
         this.baseId = baseId;
@@ -236,6 +236,6 @@ class AirtablesDownload {
 };
 
 module.exports = function (opts) {
-    const atdownload = new AirtablesDownload(opts.baseId, opts.apiKey, opts);
+    const atdownload = new AirtableDownload(opts.baseId, opts.apiKey, opts);
     atdownload.start();
 }
