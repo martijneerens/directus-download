1fa25b90472e2363d4242c68ae85f52dd18a2f9f
Martijn Eerens <martijn@eerens.nl>
2018-05-16 14:59:54 +0200

    fix csv export bug

--- airtables-download.js
+++ airtables-download.js
@@ -218,7 +218,7 @@ class AirtablesDownload {
                     that.writeBook(that.book);
 
                     if (that.opts.csvPath) {
-                        that.downloadCsv(that);
+                        that.downloadCsv(that.book);
                     }
 
                     that.downloadMedia()
